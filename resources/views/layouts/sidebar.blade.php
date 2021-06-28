45769723b11cc59e8e99e2ba5e02155f08341015
Achmad Muchlasin <muchlaseen@gmail.com>
2021-06-28 13:59:04 +0700

    Edit side & master view

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,11 +1,11 @@
 <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-            <img src="{{url('/dashboard/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+        <div class="brand-image">
+            <img src="{{url('/dashboard/dist/img/sda.png')}}" class="brand-image" style="opacity: .8" alt="User Image">
         </div>
         <div class="info">
-            <a href="#" class="d-block">Kharisma Ilham Nusantara</a>
+            <a href="#" class="d-block">Admin | Kecamatan Taman</a>
         </div>
     </div>
 
